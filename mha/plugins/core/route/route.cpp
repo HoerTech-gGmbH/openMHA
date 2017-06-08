7ade33a9b4683d49410530c7bd3bbe672f2afef6
Tobias Herzke <tobiasherzke@openmha.com>
2017-06-08 15:26:06 +0200

    remove tabs

--- mha/plugins/core/route/route.cpp
+++ mha/plugins/core/route/route.cpp
@@ -19,58 +19,58 @@ namespace route {
 
     class process_t {
     public:
-	process_t(algo_comm_t iac,
-		  const std::string acname,
-		  const std::vector<std::string>& r_out,
-		  const std::vector<std::string>& r_ac,
-		  const mhaconfig_t& cf_in,
-		  const mhaconfig_t& cf_out,
-		  const mhaconfig_t& cf_ac,
-		  bool sync);
-	mha_wave_t* process(mha_wave_t*);
-	mha_spec_t* process(mha_spec_t*);
+        process_t(algo_comm_t iac,
+                  const std::string acname,
+                  const std::vector<std::string>& r_out,
+                  const std::vector<std::string>& r_ac,
+                  const mhaconfig_t& cf_in,
+                  const mhaconfig_t& cf_out,
+                  const mhaconfig_t& cf_ac,
+                  bool sync);
+        mha_wave_t* process(mha_wave_t*);
+        mha_spec_t* process(mha_spec_t*);
     private:
-	MHAMultiSrc::waveform_t wout;
-	MHAMultiSrc::spectrum_t sout;
-	MHAMultiSrc::waveform_t wout_ac;
-	MHAMultiSrc::spectrum_t sout_ac;
+        MHAMultiSrc::waveform_t wout;
+        MHAMultiSrc::spectrum_t sout;
+        MHAMultiSrc::waveform_t wout_ac;
+        MHAMultiSrc::spectrum_t sout_ac;
     };
 
     class interface_t : public MHAPlugin::plugin_t<route::process_t>
     {
     public:
-	interface_t(algo_comm_t iac,const std::string&,const std::string&);
-	void prepare(mhaconfig_t&);
-	void release();
-	mha_wave_t* process(mha_wave_t*);
-	mha_spec_t* process(mha_spec_t*);
+        interface_t(algo_comm_t iac,const std::string&,const std::string&);
+        void prepare(mhaconfig_t&);
+        void release();
+        mha_wave_t* process(mha_wave_t*);
+        mha_spec_t* process(mha_spec_t*);
     private:
-	void update();
-	MHAEvents::patchbay_t<route::interface_t> patchbay;
-	MHAParser::vstring_t route_out;
-	MHAParser::vstring_t route_ac;
-	mhaconfig_t cfin;
-	mhaconfig_t cfout;
-	mhaconfig_t cfac;
-	bool prepared;
-	bool stopped;
-	std::string algo;
+        void update();
+        MHAEvents::patchbay_t<route::interface_t> patchbay;
+        MHAParser::vstring_t route_out;
+        MHAParser::vstring_t route_ac;
+        mhaconfig_t cfin;
+        mhaconfig_t cfout;
+        mhaconfig_t cfac;
+        bool prepared;
+        bool stopped;
+        std::string algo;
     };
 
 }
 
 route::interface_t::interface_t(algo_comm_t iac,const std::string&,const std::string& ialg)
     : MHAPlugin::plugin_t<route::process_t>(
-	"Signal router plugin.\n\n"
-	"Arguments are the input signal source names (AC variables)\n"
-	"followed by a colon, followed by the channel number, starting\n"
-	"at zero. Empty names correspond to the direct input.\n\n"
-	"An AC variable will be created if the AC output dimension is not zero.\n"
-	"Example:\n  out = [:0 :1 x:0 x:1]\n  ac = [:2 :3]\n"
-	"returns a four channel output signal containing first two\n"
-	"direct input channels, and the first two channels of the AC\n"
-	"variable \"x\". An AC variable is created with the third and\n"
-	"fourth channel of the direct input.\n\n",iac), 
+        "Signal router plugin.\n\n"
+        "Arguments are the input signal source names (AC variables)\n"
+        "followed by a colon, followed by the channel number, starting\n"
+        "at zero. Empty names correspond to the direct input.\n\n"
+        "An AC variable will be created if the AC output dimension is not zero.\n"
+        "Example:\n  out = [:0 :1 x:0 x:1]\n  ac = [:2 :3]\n"
+        "returns a four channel output signal containing first two\n"
+        "direct input channels, and the first two channels of the AC\n"
+        "variable \"x\". An AC variable is created with the third and\n"
+        "fourth channel of the direct input.\n\n",iac), 
       route_out("direct output","[]"),
       route_ac("AC output","[]"),
       prepared(false), 
@@ -103,13 +103,13 @@ void route::interface_t::release()
 void route::interface_t::update()
 {
     if( prepared )
-	push_config(new route::process_t(ac,
-					 algo,
-					 route_out.data,
-					 route_ac.data,
-					 cfin,
-					 cfout,
-					 cfac,stopped));
+        push_config(new route::process_t(ac,
+                                         algo,
+                                         route_out.data,
+                                         route_ac.data,
+                                         cfin,
+                                         cfout,
+                                         cfac,stopped));
 }
 
 mha_wave_t* route::interface_t::process(mha_wave_t* s)
@@ -125,34 +125,34 @@ mha_spec_t* route::interface_t::process(mha_spec_t* s)
 }
 
 route::process_t::process_t(algo_comm_t ac,
-			    const std::string acname,
-			    const std::vector<std::string>& r_out,
-			    const std::vector<std::string>& r_ac,
-			    const mhaconfig_t& cf_in,
-			    const mhaconfig_t& cf_out,
-			    const mhaconfig_t& cf_ac,
-			    bool sync)
+                            const std::string acname,
+                            const std::vector<std::string>& r_out,
+                            const std::vector<std::string>& r_ac,
+                            const mhaconfig_t& cf_in,
+                            const mhaconfig_t& cf_out,
+                            const mhaconfig_t& cf_ac,
+                            bool sync)
     : wout(ac,"",cf_out.fragsize, cf_out.channels),
       sout(ac,"",cf_out.fftlen/2+1, cf_out.channels),
       wout_ac(ac,acname,cf_ac.fragsize, cf_ac.channels),
       sout_ac(ac,acname,cf_ac.fftlen/2+1, cf_ac.channels)
 {
     if( r_out.size() != cf_out.channels )
-	throw MHA_Error(__FILE__,__LINE__,
-			"Expected %d entries in output route variable, got %d.",
-			cf_out.channels,r_out.size());
+        throw MHA_Error(__FILE__,__LINE__,
+                        "Expected %d entries in output route variable, got %d.",
+                        cf_out.channels,r_out.size());
     if( r_ac.size() != cf_ac.channels )
-	throw MHA_Error(__FILE__,__LINE__,
-			"Expected %d entries in AC route variable, got %d.",
-			cf_ac.channels,r_ac.size());
+        throw MHA_Error(__FILE__,__LINE__,
+                        "Expected %d entries in AC route variable, got %d.",
+                        cf_ac.channels,r_ac.size());
     wout.select_source(r_out,cf_in.channels);
     sout.select_source(r_out,cf_in.channels);
     wout_ac.select_source(r_ac,cf_in.channels);
     sout_ac.select_source(r_ac,cf_in.channels);
     if( sync && (cf_in.domain == MHA_WAVEFORM) && (cf_ac.channels > 0) )
-	wout_ac.insert();
+        wout_ac.insert();
     if( sync && (cf_in.domain == MHA_SPECTRUM) && (cf_ac.channels > 0) )
-	sout_ac.insert();
+        sout_ac.insert();
 }
 
 mha_wave_t* route::process_t::process(mha_wave_t* s)
