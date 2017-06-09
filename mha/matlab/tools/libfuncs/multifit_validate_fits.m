c8dd4ca7ded0bc3332999f533f51bb89494f5036
Hendrik Kayser <hendrikkayser@openMHA.org>
2017-06-09 16:18:34 +0200

    Fixed bug that the very first first fit did not show up in the preset list in the fitting GUI.

--- mha/matlab/tools/libfuncs/multifit_validate_fits.m
+++ mha/matlab/tools/libfuncs/multifit_validate_fits.m
@@ -1,10 +1,16 @@
 function vValid = multifit_validate_fits( csFits, sFit, mha )
+  lfa=libfitadaptor();
   sMHACfg = mha_get(mha,sFit.addr);
   vValid = ones(size(csFits));
   for k=1:length(csFits)
     try
       sFitL = csFits{k};
-      sCurrentFitmodel = merge_structs(sFitL.mha2fitmodel(sMHACfg),sFitL.fitmodel);
+      % This is ugly and relies on consinstent naming in libfitadaptor(): remove
+      % 'fitadaptor_' to get the function you want to call
+      % This hack replaces to use of function handles which cannot be saved by octave 
+      sCurrentFitmodel = merge_structs(eval(['lfa.' sFitL.mha2fitmodel(12:end) '(sMHACfg)']),sFitL.fitmodel);
+      % This is the old solution which needs a function handle
+      % sCurrentFitmodel = merge_structs(sFitL.mha2fitmodel(sMHACfg),sFitL.fitmodel);
       if ~isequal(sCurrentFitmodel,sFitL.fitmodel)
 	vValid(k) = 0;
       end
