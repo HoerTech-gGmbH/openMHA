278362b04ca0497e4d595c7bf021930fba91ef1f
Frasher Loshaj <frasherloshaj@openmha.org>
2017-06-13 15:00:00 +0200

    Add GPL licence header to user_manual documentation.

--- mha/doc/src/doc_mhamain.h
+++ mha/doc/src/doc_mhamain.h
@@ -146,7 +146,7 @@
 * measurements and adapt the openMHA settings to make sure that this
 * calibration convention is met.
 * We provide the plugin \c transducers which can be configured to perform the
-* necessary signal adjustments in most situations.
+* necessary signal adjustments.
 *
 *
 */
