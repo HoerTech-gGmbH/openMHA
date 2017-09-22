36b7867e26cf65f6e0d456f409aa6dde7b2a8957
Tobias Herzke <tobiasherzke@openmha.com>
2017-09-22 15:33:37 +0200

    More MHA_Error unit tests.

--- mha/libmha/src/mha_error_unit_tests.cpp
+++ mha/libmha/src/mha_error_unit_tests.cpp
@@ -1,7 +1,7 @@
 #include "mha_error.hh"
 #include <gtest/gtest.h>
 
-TEST(mha_error, helper_function_digits_computes_correct_number_of_decimal_digits)
+TEST(MHA_Error, helper_function_digits_computes_correct_number_of_decimal_digits)
 {
   using mha_error_helpers::digits;
   EXPECT_EQ(1U, digits(0U));
@@ -11,4 +11,10 @@ TEST(mha_error, helper_function_digits_computes_correct_number_of_decimal_digits
   EXPECT_EQ(2U, digits(99U));
   EXPECT_EQ(3U, digits(100U));
   EXPECT_EQ(10U, digits(4294967295U));
+}
+
+TEST(MHA_Error, MHA_Error_is_a_subclass_of_std_exception) {
+  MHA_Error error_instance("",0,"");
+  std::exception * std_exception_pointer = &error_instance;
+  EXPECT_NE(nullptr, std_exception_pointer);
 }
