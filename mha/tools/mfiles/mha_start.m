d2d6d25894ba5075e52e644964d43754affa0c38
tobias <tobias@windows7_32bit>
2017-09-26 11:54:29 +0200

    temporarily remove octave PATH additions from PATH when starting mha to work around library incompatibilities

--- mha/tools/mfiles/mha_start.m
+++ mha/tools/mfiles/mha_start.m
@@ -1,4 +1,3 @@
-function h = mha_start( port, pre_binary, post_binary )
 % MHA_START - start a MHA process
 %
 % Syntax:
@@ -7,7 +6,23 @@ function h = mha_start( port, pre_binary, post_binary )
 %
 % port : MHA configuration port; use [] or 0 for automatic port
 % number
+
+% This file is part of the HörTech Open Master Hearing Aid (openMHA)
+% Copyright © 2005 2006 2007 2008 2009 2011 2013 2014 2015 2017 HörTech gGmbH
 %
+% openMHA is free software: you can redistribute it and/or modify
+% it under the terms of the GNU Affero General Public License as published by
+% the Free Software Foundation, version 3 of the License.
+%
+% openMHA is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU Affero General Public License, version 3 for more details.
+%
+% You should have received a copy of the GNU Affero General Public License, 
+% version 3 along with openMHA.  If not, see <http://www.gnu.org/licenses/>.
+
+function h = mha_start( port, pre_binary, post_binary )
   h.host = 'localhost';
   next_task = 'open serversocket';
   if nargin < 1
@@ -55,18 +70,29 @@ function h = mha_start( port, pre_binary, post_binary )
     {binary, '--port', num2str(port), '--announce', num2str(my_port)};
   command = {pre_binary{:} command{:} post_binary{:}};
 
+  javaruntime = javaMethod('getRuntime', 'java.lang.Runtime');
+
+  % in octave on windows, the directories that octave adds to the PATH
+  % can contain libraries that conflict with the libraries MHA is linked
+  % with. Temporarily remove these directories from PATH to avoid conflicts.  
   if ispc() && isoctave()
+    PATH_backup = getenv('PATH');
     remove_octave_directories_from_windows_path()
   end
   
-  javaruntime = javaMethod('getRuntime', 'java.lang.Runtime');
+  % octave 4.0.0 did not translate cellstrings correctly to Java string arrays.
   octave_version = ver('Octave');
-  if( ~isempty(octave_version) && ...
-      strcmp(octave_version.Version,'4.0.0') )
-    mhaProcess = javaruntime.exec(strjoin(command,' '));
+  if( isoctave() && strcmp(octave_version.Version,'4.0.0') )
+    mhaProcess = javaruntime.exec(strjoin(command,' ')); % workaround
   else
     mhaProcess = javaruntime.exec(command);
   end
+  
+  % restore PATH
+  if ispc() && isoctave()
+    setenv('PATH', PATH_backup);
+  end
+  
   connection = acceptor.accept();
   reader = javaObject('java.io.BufferedReader', ... 
                       javaObject('java.io.InputStreamReader', ...
@@ -79,3 +105,8 @@ function h = mha_start( port, pre_binary, post_binary )
   connection.close();
   acceptor.close();
   
+% Local Variables:
+% mode: octave
+% indent-tabs-mode: nil
+% coding: utf-8-unix
+% End:
--- mha/tools/mfiles/remove_octave_directories_from_windows_path.m
+++ mha/tools/mfiles/remove_octave_directories_from_windows_path.m
@@ -1,7 +1,26 @@
+% When executing in octave on windows, remove directories containing
+% the string octave (case insensitive) from the environment variable PATH
+% unless the global variable KEEP_OCTAVE_DIRECTORIES_IN_PATH is truthy.
+% 
+% functionality can be switched off by setting global variable
+% KEEP_OCTAVE_DIRECTORIES_IN_PATH to true
+
+% This file is part of the HörTech Open Master Hearing Aid (openMHA)
+% Copyright © 2017 HörTech gGmbH
+%
+% openMHA is free software: you can redistribute it and/or modify
+% it under the terms of the GNU Affero General Public License as published by
+% the Free Software Foundation, version 3 of the License.
+%
+% openMHA is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU Affero General Public License, version 3 for more details.
+%
+% You should have received a copy of the GNU Affero General Public License, 
+% version 3 along with openMHA.  If not, see <http://www.gnu.org/licenses/>.
+
 function remove_octave_directories_from_windows_path()
-  % When executing in octave on windows, remove directories containing
-  % the string octave (case insensitive) from the environment variable PATH
-  % unless the global variable KEEP_OCTAVE_DIRECTORIES_IN_PATH is truthy.
 
   % functionality can be switched off by setting this global variable to true
   global KEEP_OCTAVE_DIRECTORIES_IN_PATH;
@@ -12,11 +31,22 @@ function remove_octave_directories_from_windows_path()
   if ispc() && isoctave()
 				% We are in octave on windows
     PATH = getenv('PATH');
-
-   % Quick and HARMFUL hack: replace octave with an unlikely
-   % string. This is harmful because it might place things in the path
-   % that we do not want to have in there.
-    PATH = regexprep(PATH, 'octave', 'NICHTS', 'ignorecase');
-
+    delimiter = ';'; % Windows uses semicolon to separate directories in PATH
+    
+    % split the path into individual directories
+    PATH_directories = split_string(PATH, delimiter);
+    
+    % remove all directories that point into the octave installation
+    PATH_directories = ...
+      remove_matching_entries_from_cellstring(PATH_directories, OCTAVE_HOME);
+    
+    % rebuild the PATH with our adjustments
+    PATH = strjoin(PATH_directories, delimiter);
     setenv('PATH', PATH);
   end
+
+% Local Variables:
+% mode: octave
+% indent-tabs-mode: nil
+% coding: utf-8-unix
+% End:
