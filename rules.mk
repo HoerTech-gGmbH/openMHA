4bff8524d012eb2b37af74db1be64fe5afd0eb7c
Paul Maanen <paulmaanen@openMHA.org>
2017-12-07 14:50:37 +0100

    Fix installation error when targets already exist

--- rules.mk
+++ rules.mk
@@ -65,20 +65,6 @@ clean:
 	rm -Rf $(BUILD_DIR)
 	for m in $(SUBDIRS); do $(MAKE) -C $$m clean; done
 
-unit-tests: execute-unit-tests $(patsubst %,%-subdir-unit-tests,$(SUBDIRS))
-
-$(patsubst %,%-subdir-unit-tests,$(SUBDIRS)):
-	$(MAKE) -C $(@:-subdir-unit-tests=) unit-tests
-
-execute-unit-tests: $(BUILD_DIR)/unit-test-runner
-	if [ -x $< ]; then $<; fi
-
-unit_tests_test_files = $(wildcard $(SOURCE_DIR)/*_unit_tests.cpp)
-
-$(BUILD_DIR)/unit-test-runner: $(unit_tests_test_files) $(patsubst %_unit_tests.cpp, %.cpp , $(unit_tests_test_files))
-	@echo dependencies = $^
-	$(CXX) $(CXXFLAGS) --coverage -o $@ $^ $(LDFLAGS) $(patsubst -lopenmha,,$(LDLIBS)) -lgmock_main -lpthread
-
 # Static Pattern Rule defines standard prerequisites for plugins
 $(PLUGINS:%=$(BUILD_DIR)/%$(PLUGIN_EXT)): %$(PLUGIN_EXT): %.o
 
