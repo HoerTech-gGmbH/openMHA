4dbb6c981982c7638b8d88024a7b873134856bc2
Tobias Herzke <tobiasherzke@openmha.com>
2018-05-10 15:29:32 +0200

    Enhance camfit rule
    Closes T152
    
    Ben Williges has suggested these points, and I believe this change
    implements them:
    
    * Derzeit ist noch ein Noisegate ab 45 dB per default
      eingestellt. Dies hat zur Folge, dass bei 0 dB HL über alle
      Frequenzen z.T. eine Abschwächung des Ausgangssignales stattfindet
    
    * Bitte fügt noch einen Kommentar hinzu, wie genau der Gain oberhalb
      von 5 kHz in CAMFIT compressiv und CAMFIT linear interpoliert wird
      (in der orginal-publikation ist der frequenz-abhängigen Intercept
      nur bis 5 Khz definiert)
    
    * Evtl. könnte es nützlich sein einen maximalen Gain einzuführen,
      derzeit wird in CAMFIT einfach immer weiter verstärkt... (Die Anzahl
      der Leute, die wirklich Power Output jenseits 100 dB brauchen sind
      wahrscheinlich limitiert und ähnlich wie bei den Fitting-Softwares
      für Hörgeräte/Audiometer, kann eine einfache Meldung dann
      ausreichen, dass ein extended Mode oder eine spezielle Konfiguration
      aktiviert werden muss, um Output jenseits von 100 dB zuzulassen.

--- mha/tools/mfiles/gainrule_camfit_linear.m
+++ mha/tools/mfiles/gainrule_camfit_linear.m
@@ -12,7 +12,14 @@ function sGt = gainrule_camfit_linear(sAud, sFitmodel)
 % Linear cambridge rule for hearing aid fittings.  
 % Implemented as described in B. Moore (1998), "Use of a loudness model for 
 % hearing-aid fitting. I. Linear hearing aids" Brit. J. Audiol. (32) 317-335
-
+%
+% Please note that this gain rule by default applies a noise gate below 45 dB
+% band level.  If you want a different noise gate level, please set the global
+% variable CAMFIT_NOISEGATE before this function is called.  Please note that
+% the gain rule now limits the gains so that in each band 100 dB output level
+% is not exceeded. If you need higher output levels, please set the global
+% variable CAMFIT_MAXOUT before this function is called.
+  
 % This file is part of the HörTech Open Master Hearing Aid (openMHA)
 % Copyright © 2007 2011 2013 2015 2016 2017 HörTech gGmbH
 %
@@ -30,6 +37,10 @@ function sGt = gainrule_camfit_linear(sAud, sFitmodel)
 
   libaudprof();
 
+  % The cambridge formula defines intercepts only up to 5 kHz. Because
+  % the intercepts do not vary much between 1kHz and 5kHz anyway (these
+  % are all within 0dB ± 1dB), we extend the last intercept of +1dB at
+  % 5kHz to all higher frequencies.
   intercept_frequencies = ...
       [125 250 500 750 1000 1500 2000 3000 4000 5000 5005];
   intercepts = [-11 -10 -8 -6 0 -1 1 -1 0 1 1];
@@ -43,6 +54,12 @@ function sGt = gainrule_camfit_linear(sAud, sFitmodel)
       noisegate = CAMFIT_NOISEGATE;
   end
 
+  max_output_level = 100;
+  global CAMFIT_MAXOUT;
+  if ~isempty(CAMFIT_MAXOUT)
+    max_output_level = CAMFIT_MAXOUT;
+  end
+  
   % Interpolate audiogram
   for side=sFitmodel.side
      sT = audprof.threshold_get(sAud, side, 'htl_ac');
@@ -60,6 +77,12 @@ function sGt = gainrule_camfit_linear(sAud, sFitmodel)
     insertion_gains.(side) = insertion_gains.(side) * any(htl.(side));
     
     sGt.(side) = repmat(insertion_gains.(side), length(sFitmodel.levels),1);
+
+    % where output level is greater than max_output_level, reduce gain
+    output_levels = sGt.(side) + repmat(sFitmodel.levels(:),1,length(insertion_gains.(side)));
+    safe_output_levels = min(output_levels, max_output_level);
+    sGt.(side) = sGt.(side) - (output_levels - safe_output_levels);
+    
     sGt.noisegate.(side).level = noisegate*ones(size(sFitmodel.frequencies));
     sGt.noisegate.(side).slope = ones(size(sFitmodel.frequencies));
   end
