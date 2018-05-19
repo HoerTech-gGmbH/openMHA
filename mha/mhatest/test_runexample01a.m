b04a65e7ecd5053e79a4803413e0f0bc24d9414d
tobias <tobiasherzke@openmha.com>
2018-05-19 12:51:33 +0200

    Fix help text for test_runexample01a.m

--- mha/mhatest/test_runexample01a.m
+++ mha/mhatest/test_runexample01a.m
@@ -1,28 +1,21 @@
-## This file is part of the HörTech Open Master Hearing Aid (openMHA)
-## Copyright © 2018 HörTech gGmbH
-##
-## openMHA is free software: you can redistribute it and/or modify
-## it under the terms of the GNU Affero General Public License as published by
-## the Free Software Foundation, version 3 of the License.
-##
-## openMHA is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU Affero General Public License, version 3 for more details.
-##
-## You should have received a copy of the GNU Affero General Public License, 
-## version 3 along with openMHA.  If not, see <http://www.gnu.org/licenses/>.
+# Execute MHA with example configuration 
+# mha/examples/01-dynamic-compression/example_dc.cfg and check that the 
+# expected output sound file is produced.
 
-## -*- texinfo -*- 
-## @deftypefn {} test_runexample01a ()
-## Execute MHA with example configuration 
-## mha/examples/01-dynamic-compression/example_dc.cfg and check that the 
-## expected output sound file is produced.
-## @seealso{}
-## @end deftypefn
-
-## Author: tobias <tobias@openmha.com>
-## Created: 2018-05-19
+# This file is part of the HörTech Open Master Hearing Aid (openMHA)
+# Copyright © 2018 HörTech gGmbH
+#
+# openMHA is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# openMHA is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License, version 3 for more details.
+#
+# You should have received a copy of the GNU Affero General Public License, 
+# version 3 along with openMHA.  If not, see <http://www.gnu.org/licenses/>.
 
 function test_runexample01a()
   dir = '../examples/01-dynamic-compression/';
