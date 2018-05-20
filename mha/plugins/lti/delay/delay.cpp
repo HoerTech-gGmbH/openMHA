24e1b1e4520bb86857eb997f170300df8da3db95
tobias <tobiasherzke@openmha.com>
2018-05-20 13:42:48 +0200

    openMHA integration

--- mha/plugins/lti/delay/delay.cpp
+++ mha/plugins/lti/delay/delay.cpp
@@ -4,7 +4,7 @@
 // openMHA is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
 // the Free Software Foundation, version 3 of the License.
-// 
+//
 // openMHA is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
