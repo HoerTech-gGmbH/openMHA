6061e96aeae2112f70a12609f33a97970033028e
tobias <tobiasherzke@openmha.com>
2018-06-25 13:41:20 +0200

    Fix peaklevel. Add example config

--- mha/tools/mfiles/fresponse_to_cfg.m
+++ mha/tools/mfiles/fresponse_to_cfg.m
@@ -9,11 +9,11 @@ function [peak,fir] = fresponse_to_cfg(fresponse)
   gains = fresponse.dBFSfor80dB + fresponse.REUGdB + fresponse.correctionsdB;
   peak_correction = max(gains);
   gains = gains - peak_correction;
-  peak = 0  - peak_correction + 93.9794;
+  peak = 0  - peak_correction + 80; % the level we used for calibration
   fir = fir2(fir_length, [0 (fresponse.Frequencies / fresponse.sampling_rate * 2) 1], 10.^([gains(1), gains, -inf] / 20));
 
   printf("The following values can be configured into the transducers plugin for the audio output channel where this output hardware is connected:\n");
-  printf("calib_out.peak = [... %f ...]\n", peak);
+  printf("calib_out.peaklevel = [... %f ...]\n", peak);
   printf("calib_out.fir = [... ;[");
   printf("%.7f ", fir);
   printf("]; ...]\n");
