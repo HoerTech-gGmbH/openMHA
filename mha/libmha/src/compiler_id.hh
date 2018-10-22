0dd055fcec87eaad26a7ca4090fdebbad3aedb3f
tobias <tobias@tobiass-Mac-mini.local>
2018-10-22 19:05:15 +0200

    Compiler identification
    Summary:
    Defines a macro COMPILER_ID that evaluates to a compile-time-constant string which identifies compiler, its version,
    and the C++ standard, as "gcc-8.2.0-c++14" or "clang-6.0.0-c++11".
    
    We want to include a variable that returns this string into every plugin, libopenmha.so, and mha.exe.
    In future, MHA will refuse to load plugins compiled with different compilers. Plugins will refuse to link to
    libopenmha compiled with different compiler.
    
    No support for compilers other than gcc or clang, no support for C++ standards earlier than c++11.
    
    Unit tests are provided for the building blocks.
    
    On the way, I also fixed the compilation of external libraries: They are now compiled with the same compiler as the MHA.
    
    Closes T307.
    
    Test Plan:
    * Apply Patch
    * Compile MHA with gcc
    * Execute unit-tests
      * Verify unit tests in directory mha/libmha succeed.
      * Watch out for additional printout in the form COMPILER_ID=... and verify that it is correct
    * Repeat test with clang, preferably on Mac. I have done clang already on linux.
    
    Reviewers: pmaanen
    
    Reviewed By: pmaanen
    
    Subscribers: tobias
    
    Maniphest Tasks: T307
    
    Differential Revision: https://dev.openmha.org/D36

---
+++ mha/libmha/src/compiler_id.hh
@@ -0,0 +1,51 @@
+// This file is part of the HörTech Open Master Hearing Aid (openMHA)
+// Copyright © 2018 HörTech gGmbH
+//
+// openMHA is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, version 3 of the License.
+//
+// openMHA is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License, version 3 for more details.
+//
+// You should have received a copy of the GNU Affero General Public License, 
+// version 3 along with openMHA.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifdef __clang__
+# define COMPILER_ID_VENDOR "clang"
+# define COMPILER_ID_MAJOR __clang_major__
+# define COMPILER_ID_MINOR __clang_minor__
+# define COMPILER_ID_PATCH __clang_patchlevel__
+#else
+# define COMPILER_ID_VENDOR "gcc"
+# define COMPILER_ID_MAJOR __GNUC__
+# define COMPILER_ID_MINOR __GNUC_MINOR__
+# define COMPILER_ID_PATCH __GNUC_PATCHLEVEL__
+#endif
+#define COMPILER_ID_VERSION_HELPER2(x,y,z) #x "." #y "." #z
+#define COMPILER_ID_VERSION_HELPER1(x,y,z) COMPILER_ID_VERSION_HELPER2(x,y,z)
+#define COMPILER_ID_VERSION COMPILER_ID_VERSION_HELPER1(COMPILER_ID_MAJOR, \
+                                                        COMPILER_ID_MINOR, \
+                                                        COMPILER_ID_PATCH)
+#if __cplusplus == 201103L
+# define COMPILER_ID_STANDARD "c++11"
+#endif
+#if __cplusplus == 201402L
+# define COMPILER_ID_STANDARD "c++14"
+#endif
+#if __cplusplus == 201703L
+# define COMPILER_ID_STANDARD "c++17"
+#endif
+
+// The compiler id will be sth like "gcc-8.2.0-c++14" or "clang-6.0.0-c++17"
+#define COMPILER_ID \
+  COMPILER_ID_VENDOR "-" COMPILER_ID_VERSION "-" COMPILER_ID_STANDARD
+
+// Local Variables:
+// compile-command: "make -C .."
+// coding: utf-8-unix
+// c-basic-offset: 2
+// indent-tabs-mode: nil
+// End:
---
+++ mha/libmha/src/compiler_id_unit_tests.cpp
@@ -0,0 +1,52 @@
+// This file is part of the HörTech Open Master Hearing Aid (openMHA)
+// Copyright © 2018 HörTech gGmbH
+//
+// openMHA is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, version 3 of the License.
+//
+// openMHA is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License, version 3 for more details.
+//
+// You should have received a copy of the GNU Affero General Public License, 
+// version 3 along with openMHA.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <gtest/gtest.h>
+#include "compiler_id.hh"
+#include <cstdio>
+// we test our own compiler detection against the one from boost
+#include <boost/predef.h>
+
+TEST(compiler_id, compiler_vendor_and_version)
+{
+  if (BOOST_COMP_GNUC) {
+    EXPECT_STREQ("gcc", COMPILER_ID_VENDOR);
+    EXPECT_EQ(BOOST_VERSION_NUMBER_MAJOR(BOOST_COMP_GNUC), COMPILER_ID_MAJOR);
+    EXPECT_EQ(BOOST_VERSION_NUMBER_MINOR(BOOST_COMP_GNUC), COMPILER_ID_MINOR);
+    EXPECT_EQ(BOOST_VERSION_NUMBER_PATCH(BOOST_COMP_GNUC), COMPILER_ID_PATCH);
+    EXPECT_STREQ(__VERSION__, COMPILER_ID_VERSION);
+  }
+  else if (BOOST_COMP_CLANG) {
+    EXPECT_STREQ("clang", COMPILER_ID_VENDOR);
+    EXPECT_EQ(BOOST_VERSION_NUMBER_MAJOR(BOOST_COMP_CLANG), COMPILER_ID_MAJOR);
+    EXPECT_EQ(BOOST_VERSION_NUMBER_MINOR(BOOST_COMP_CLANG), COMPILER_ID_MINOR);
+    EXPECT_EQ(BOOST_VERSION_NUMBER_PATCH(BOOST_COMP_CLANG), COMPILER_ID_PATCH);
+    EXPECT_EQ(0, strncmp(__clang_version__, COMPILER_ID_VERSION,
+                         strlen(COMPILER_ID_VERSION)));
+  }
+  else {
+    EXPECT_STRNE("gcc", COMPILER_ID_VENDOR);
+    EXPECT_STRNE("clang", COMPILER_ID_VENDOR);
+    FAIL(/* unexpected compiler */);
+  }
+  printf("COMPILER_ID=%s\n", COMPILER_ID);
+}
+
+// Local Variables:
+// compile-command: "make -C .. unit-tests"
+// coding: utf-8-unix
+// c-basic-offset: 2
+// indent-tabs-mode: nil
+// End:
