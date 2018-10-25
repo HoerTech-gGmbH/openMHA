4d36e3454643a9dc88e468cf01e55a8ba11e78cc
Paul Maanen <paulmaanen@openMHA.org>
2018-10-25 19:15:14 +0200

    Fix compiler id unit tests
    Unit tests now compile with older versions of boost and check
    for possibly undefined symbols and defines them with dummy values.
    
    Resolves T325

--- mha/libmha/src/compiler_id_unit_tests.cpp
+++ mha/libmha/src/compiler_id_unit_tests.cpp
@@ -19,6 +19,30 @@
 // we test our own compiler detection against the one from boost
 #include <boost/predef.h>
 
+/* Older boost versions do not have these macros, so define them if needed. */
+#ifndef BOOST_VERSION_NUMBER_MAJOR
+#define BOOST_VERSION_NUMBER_MAJOR(N) \
+    ( ((N)/10000000)%100 )
+#endif //BOOST_VERSION_NUMBER_MAJOR
+
+#ifndef BOOST_VERSION_NUMBER_MINOR
+#define BOOST_VERSION_NUMBER_MINOR(N) \
+    ( ((N)/100000)%100 )
+#endif //BOOST_VERSION_NUMBER_MINOR
+
+#ifndef BOOST_VERSION_NUMBER_PATCH
+#define BOOST_VERSION_NUMBER_PATCH(N) \
+( (N)%100000 )
+#endif //BOOST_VERSION_NUMBER_PATCH
+
+#ifndef __clang_version__
+#define __clang_version__ "0"
+#endif
+
+#ifndef __VERSION__
+#define  __VERSION__ "0"
+#endif
+
 TEST(compiler_id, compiler_vendor_and_version)
 {
   if (BOOST_COMP_GNUC) {
@@ -26,7 +50,8 @@ TEST(compiler_id, compiler_vendor_and_version)
     EXPECT_EQ(BOOST_VERSION_NUMBER_MAJOR(BOOST_COMP_GNUC), COMPILER_ID_MAJOR);
     EXPECT_EQ(BOOST_VERSION_NUMBER_MINOR(BOOST_COMP_GNUC), COMPILER_ID_MINOR);
     EXPECT_EQ(BOOST_VERSION_NUMBER_PATCH(BOOST_COMP_GNUC), COMPILER_ID_PATCH);
-    EXPECT_STREQ(__VERSION__, COMPILER_ID_VERSION);
+    EXPECT_EQ(0, strncmp(__VERSION__, COMPILER_ID_VERSION,
+                         strlen(COMPILER_ID_VERSION)));
   }
   else if (BOOST_COMP_CLANG) {
     EXPECT_STREQ("clang", COMPILER_ID_VENDOR);
