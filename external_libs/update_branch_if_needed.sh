f9ebb5b7a896eea0ea5cdf6c5cb4eef8daab55f1
Tobias Herzke <tobiasherzke@openmha.com>
2019-01-03 23:21:15 +0100

    Update the external_libs branches when there was an update in external_libs

--- external_libs/update_branch_if_needed.sh
+++ external_libs/update_branch_if_needed.sh
@@ -1,6 +1,7 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
 # A shell script that determines if the external_libs branch needs an update
+# and if yes, updates it
 
 # if we cannot get the current branch name, exit with false (option -e exits)
 git rev-parse --abbrev-ref HEAD
@@ -16,5 +17,12 @@ git rev-parse --verify --quiet external_libs/"$branch_name"
 # We need to determine if the external_libs directory (this is the directory
 # that contains this script) has any content differences between these two.
 extlibsdir=$(dirname "$0")
-git diff --exit-code --quiet external_libs/"$branch_name" "$branch_name" \
-         -- "$extlibsdir"
+if ! git diff --exit-code --quiet external_libs/"$branch_name" "$branch_name" \
+              -- "$extlibsdir"
+then
+    # There are differences, we need to fast-forward the external_libs branch
+    git checkout external_libs/"$branch_name"
+    git merge --ff-only
+    # And publish the new external_libs branch to the repository
+    git push
+fi
