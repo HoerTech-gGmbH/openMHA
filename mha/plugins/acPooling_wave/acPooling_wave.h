a5f81c94d24918598a5268ac9da1badebaff73bc
Kamil Adiloglu <k.adiloglu@hoertech.de>
2019-01-11 11:00:20 +0100

    First, the documentation has been extended. In this version, the configuration variables are explained in a more detail. There is e short example configuration within the context of localization.
    Second, I added some checks to make sure that the pooling window
    size is at least twice the size of the fragsize so that there are
    at least two frames to be able to compute the average.

--- mha/plugins/acPooling_wave/acPooling_wave.h
+++ mha/plugins/acPooling_wave/acPooling_wave.h
@@ -14,6 +14,21 @@
 // version 3 along with openMHA.  If not, see <http://www.gnu.org/licenses/>.
 
 #ifndef ACPOOLING_WAVE_H
+// This file is part of the HörTech Open Master Hearing Aid (openMHA)
+// Copyright © 2015 2018 HörTech gGmbH
+//
+// openMHA is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, version 3 of the License.
+//
+// openMHA is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License, version 3 for more details.
+//
+// You should have received a copy of the GNU Affero General Public License, 
+// version 3 along with openMHA.  If not, see <http://www.gnu.org/licenses/>.
+
 #define ACPOOLING_WAVE_H
 
 #include "mha_plugin.hh"
