a29fe3fb96b6d6c78a01f21350d273d0c5bcea02
Paul Maanen <paulmaanen@openmha.org>
2019-08-01 17:11:14 +0200

    Revert "ac2wave: Clarify documentation. Closes T76."
    This reverts commit db7556e7c1f45582b4d01afd695b3ce2406d7858.

--- mha/plugins/ac2wave/ac2wave.cpp
+++ mha/plugins/ac2wave/ac2wave.cpp
@@ -106,8 +106,7 @@ ac2wave_if_t::ac2wave_if_t(const algo_comm_t& iac,
     : MHAPlugin::plugin_t<ac2wave_t>(
         "Mix the main input signal with a waveform stored into AC\n"
         "variables. Main and AC signal can be attenuated or delayed\n"
-        "by integer fragments. The AC variable and the input waveform have to\n"
-        "have the same dimensions.\n\n"
+        "by integer fragments.\n\n"
         "Spectral input is discarded and replaced by a zero signal.\n\n",iac),
       name("AC variable name",""),
       gain_in("Linear gain for main input signal","0"),
