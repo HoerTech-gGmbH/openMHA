7e785b9e030561e6f6c542b232d4d32cd16fbb10
Paul Maanen <paulmaanen@openmha.org>
2019-12-02 13:31:45 +0100

    Add tool to translate frequency response to equaliser configuration
    Summary: mfiles: Add tool to translate frequency response to equaliser configuration
    
    Test Plan: Manual review, usage for headset calibration.
    
    Reviewers: tobias
    
    Reviewed By: tobias
    
    Maniphest Tasks: T837
    
    Differential Revision: https://dev.openmha.org/D253

--- mha/tools/mfiles/fresponse_to_eq.m
+++ mha/tools/mfiles/fresponse_to_eq.m
@@ -1,8 +1,17 @@
 function [peak,eq] = fresponse_to_eq(fresponse,srate,fftlen,print_result)
 % function fresponse_to_eq(fresponse)
-% fresponse: struct with fields
-% Frequencies, dBFSfor80dB, correctionsdB, REUG as created by measure_fresponse
-%
+% computes peak level and equalizer settings from fresponse
+% INPUT:
+% fresponse: struct with fields "Frequencies", "dBFSfor80dB",
+%            "correctionsdB", "REUG" as created by measure_fresponse
+% srate:     sampling rate / Hz
+% fftlen:    FFT length in samples
+% print_result: Optional boolean parameter. Suggested MHA settings will be
+%               printed when true
+% OUTPUT:
+% peak: suggested output peak level from measurement and correction data
+% eq:   suggested equalizer setting to apply the correction filter.
+
 % This file is part of the HörTech Open Master Hearing Aid (openMHA)
 % Copyright © 2019 HörTech gGmbH
 %
@@ -24,6 +33,7 @@ function [peak,eq] = fresponse_to_eq(fresponse,srate,fftlen,print_result)
   peak = 0  - peak_correction + 80; % the level we used for calibration
   frequencies=linspace(0,srate/2,fftlen/2+1);
   eq=interp1(fresponse.Frequencies,gains,frequencies,'linear','extrap');
+  eq = 10.^(eq/20);
   if ~exist('print_result','var')
     print_result=false;
   endif
@@ -31,6 +41,6 @@ function [peak,eq] = fresponse_to_eq(fresponse,srate,fftlen,print_result)
      printf("The following values can be configured into the transducers plugin for the audio output channel where this output hardware is connected:\n");
      printf("calib_out.peaklevel = [... %f ...]\n", peak);
      printf("equalize.gains=[...;[");
-     printf("%.2f ",10.^(eq/20));
+     printf("%.2f ",eq);
      printf("]; ...]\n");
   endif
