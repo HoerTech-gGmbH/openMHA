df7909c9465f054bd714a706f20908d5a8a38bd1
tobias <tobiasherzke@openmha.com>
2020-11-27 18:18:34 +0100

    wavrec: Make output sample format of wavrec user-configurable
    Summary:
     * wavrec: Make output sample format of wavrec user-configurable
     * I copied the code of MHAIOFile for this
    Closes T1392
    
    Test Plan: Manual review
    
    Reviewers: hendrikk, tobias
    
    Reviewed By: tobias
    
    Maniphest Tasks: T1392
    
    Differential Revision: https://dev.openmha.org/D490

--- mha/plugins/wavrec/wavrec.cpp
+++ mha/plugins/wavrec/wavrec.cpp
@@ -24,7 +24,7 @@
 class wavwriter_t {
 public:
     wavwriter_t(bool active,const mhaconfig_t& cf,unsigned int fifosize,unsigned int minwrite,
-                const std::string& prefix,bool use_date);
+                const std::string& prefix,bool use_date, const std::string& format_name_);
     ~wavwriter_t();
     void process(mha_wave_t*);
     void exit_request();
@@ -32,6 +32,13 @@ private:
     static void* write_thread(void* this_){((wavwriter_t*)this_)->write_thread();return NULL;};
     void write_thread();
     void create_soundfile(const std::string& prefix, bool use_date);
+    /** Converts the format_name string to the corresponding int according to libsndfile and
+     *  writes it into the format field of sf_info
+     *  throws if no format of this name is available
+     * @param sf_info Destination sf_info struct for the format
+     * @throw MHA_Error If no sample format of name format_name is offered by libsndfile
+     */
+    void set_format(SF_INFO& sf_info);
     std::atomic<bool> close_session;
     bool act_;
     mhaconfig_t cf_;
@@ -40,6 +47,7 @@ private:
     unsigned int minw_;
     pthread_t writethread;
     float* data;
+    std::string format_name;
 };
 
 class wavrec_t : public MHAPlugin::plugin_t<wavwriter_t> {
@@ -55,27 +63,44 @@ private:
     MHAParser::int_t minwrite;
     MHAParser::string_t prefix;
     MHAParser::bool_t use_date;
+    MHAParser::kw_t output_sample_format;
     MHAEvents::patchbay_t<wavrec_t> patchbay;
 };
 
-wavrec_t::wavrec_t(const algo_comm_t& iac,const std::string&,const std::string& algo_name)
-    : MHAPlugin::plugin_t<wavwriter_t>("wav file recorder",iac),
-      record("Record session. Each write access with argument \"yes\" will start a\n"
-             "new file with current time and date as a name.","no"),
-      fifolen("Length of FIFO in samples","262144","[2,]"),
-      minwrite("Minimal write length (must be less then fifolen)","65536","[1,]"),
-      prefix("Path (including path delimiter) and file prefix",""),
-      use_date("Use date and time (yes), or only prefix (no)","yes")
-{
-    // make the plug-in findable via "?listid"
-    set_node_id(algo_name);
+wavrec_t::wavrec_t(const algo_comm_t &iac, const std::string &,
+                   const std::string &algo_name)
+    : MHAPlugin::plugin_t<wavwriter_t>("wav file recorder", iac),
+      record("Record session. Each write access with argument \"yes\" will "
+             "start a\n"
+             "new file with current time and date as a name.",
+             "no"),
+      fifolen("Length of FIFO in samples", "262144", "[2,]"),
+      minwrite("Minimal write length (must be less then fifolen)", "65536",
+               "[1,]"),
+      prefix("Path (including path delimiter) and file prefix", ""),
+      use_date("Use date and time (yes), or only prefix (no)", "yes"),
+      output_sample_format("Output sample format", "32_bit_float",
+                           "[32_bit_float]") {
+  // make the plug-in findable via "?listid"
+  set_node_id(algo_name);
 
-    insert_member(fifolen);
-    insert_member(minwrite);
-    insert_member(prefix);
-    insert_member(use_date);
-    insert_member(record);
-    patchbay.connect(&record.writeaccess,this,&wavrec_t::start_new_session);
+  insert_member(fifolen);
+  insert_member(minwrite);
+  insert_member(prefix);
+  insert_member(use_date);
+  insert_member(record);
+  insert_member(output_sample_format);
+  patchbay.connect(&record.writeaccess, this, &wavrec_t::start_new_session);
+  int count(0);
+  sf_command(NULL, SFC_GET_FORMAT_SUBTYPE_COUNT, &count, sizeof(int));
+  for (int k = 0; k < count; k++) {
+    SF_FORMAT_INFO format_info;
+    format_info.format = k;
+    sf_command(NULL, SFC_GET_FORMAT_SUBTYPE, &format_info, sizeof(format_info));
+    std::string fmtname(format_info.name);
+    MHAParser::strreplace(fmtname, " ", "_");
+    output_sample_format.data.add_entry(fmtname);
+  }
 }
 
 mha_wave_t* wavrec_t::process(mha_wave_t* s)
@@ -101,7 +126,7 @@ void wavrec_t::start_new_session()
     auto latest_cfg=peek_config();
     if(latest_cfg)
         latest_cfg->exit_request();
-    push_config(new wavwriter_t(record.data,input_cfg(),fifolen.data,minwrite.data,prefix.data,use_date.data));
+    push_config(new wavwriter_t(record.data,input_cfg(),fifolen.data,minwrite.data,prefix.data,use_date.data,output_sample_format.data.get_value()));
 }
 
 void wavwriter_t::create_soundfile(const std::string& prefix, bool use_date)
@@ -131,21 +156,25 @@ void wavwriter_t::create_soundfile(const std::string& prefix, bool use_date)
     memset(&sfinfo,0,sizeof(sfinfo));
     sfinfo.samplerate = (int)cf_.srate;
     sfinfo.channels = cf_.channels;
-    sfinfo.format = SF_FORMAT_WAV|SF_FORMAT_FLOAT|SF_ENDIAN_FILE;
+
+    sfinfo.format = SF_FORMAT_WAV;
+    set_format(sfinfo);
     sf = sf_open(fname.c_str(),SFM_WRITE,&sfinfo);
+    sf_command(sf, SFC_SET_CLIPPING, NULL, SF_TRUE);
     if( !sf )
         throw MHA_Error(__FILE__,__LINE__,"Unable to create sound file %s: %s",fname.c_str(),sf_strerror(sf));
 }
 
 wavwriter_t::wavwriter_t(bool active,const mhaconfig_t& cf,unsigned int fifosize,unsigned int minwrite,
-                         const std::string& prefix, bool use_date)
+                         const std::string& prefix, bool use_date, const std::string& format_name_)
     : close_session(false),
       act_(active),
       cf_(cf),
       sf(NULL),
       fifo(fifosize),
       minw_(minwrite),
-      data(new float[fifosize])
+      data(new float[fifosize]),
+      format_name(format_name_)
 {
     if(minw_ >= fifosize )
         throw MHA_Error(__FILE__,__LINE__,"minwrite must be less then fifosize (minwrite: %u, fifosize: %u)",minw_,fifosize);
@@ -199,6 +228,26 @@ void wavwriter_t::write_thread()
     }
 }
 
+void wavwriter_t::set_format(SF_INFO& sfinfo) {
+  int count(0);
+  sf_command(NULL, SFC_GET_FORMAT_SUBTYPE_COUNT, &count, sizeof(int));
+  for (int k = 0; k < count; k++) {
+    SF_FORMAT_INFO format_info;
+    format_info.format = k;
+    sf_command(NULL, SFC_GET_FORMAT_SUBTYPE, &format_info, sizeof(format_info));
+    std::string tmp(format_info.name);
+    MHAParser::strreplace(tmp, " ", "_");
+    if (format_name == tmp) {
+      sfinfo.format =
+          (sfinfo.format & (SF_FORMAT_TYPEMASK | SF_FORMAT_ENDMASK)) |
+          format_info.format;
+      return ;
+    }
+  }
+  // This must be a bug in the implementation as the range of the cfg variable is restricted to
+  // the list of available formats of libsndfile
+  throw MHA_Error(__FILE__,__LINE__,"Bug: Could not find format: %s",format_name.c_str());
+}
 
 MHAPLUGIN_CALLBACKS(wavrec,wavrec_t,wave,wave)
 MHAPLUGIN_DOCUMENTATION\
