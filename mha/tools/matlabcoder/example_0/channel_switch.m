2db28e677d3a737a4781f674a5dfb4519b31766c
Paul Maanen <paulmaanen@openMHA.org>
2021-02-15 15:35:04 +0100

    matlabcoder: Re-arrange files
    Deleted tools/matlabcoder/example_0 as the code was already in example 23
    Moved example_1 and example_2 to examples directory and renamed them accordingly.
    This saves us editing the packaging.

--- mha/tools/matlabcoder/example_0/channel_switch.m
+++
@@ -1,5 +0,0 @@
-function switched_signal = channel_switch(signal_frag)
-    % Function takes a 2 channel signal fragment and switches the channels
-    A = [0 1; 1 0];
-    switched_signal = A * signal_frag;
-end
--- mha/tools/matlabcoder/example_0/process.m
+++
@@ -1,5 +0,0 @@
-function signal_frag_out = process(signal_frag_in)
-
-signal_frag_out = channel_switch(signal_frag_in);
-
-end
