b348f10abe125a5f14e19057bb75afdb4aec2fa1
MaxZ <m.zimmermann@hoertech.de>
2022-02-24 15:36:57 +0100

    Revert "Increase version number to 4.17.0"
    This reverts commit 8616636957e0fc0f9f7d55c6dfe2b01f80569e4a.

--- mha/libmha/src/mha.hh
+++ mha/libmha/src/mha.hh
@@ -45,10 +45,10 @@
 #define MHA_VERSION_MAJOR 4
 
 /** Minor version number of MHA */
-#define MHA_VERSION_MINOR 17
+#define MHA_VERSION_MINOR 16
 
 /** Release number of MHA */
-#define MHA_VERSION_RELEASE 0
+#define MHA_VERSION_RELEASE 1
 
 /** Build number of MHA (currently unused) */
 #define MHA_VERSION_BUILD 0
