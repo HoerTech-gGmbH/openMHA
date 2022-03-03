842b6869c853f43e5618b4d7f578ea4f79f6648d
Paul Maanen <paul.maanen@gmail.com>
2022-03-03 13:01:39 +0100

    Add documentation to ac2wave plugin.
    Summary:
    I added some doxygen documentation to the implementation and some latex documentation
    for the manual, while at it I surrounded the plugin with its namespace per current
    practice and replaced a unneeded heap allocation with a plain data member.
    
    Closes T76.
    
    Test Plan: Manual review
    
    Reviewers: tobias
    
    Reviewed By: tobias
    
    Maniphest Tasks: T76
    
    Differential Revision: https://dev.openmha.org/D691

--- mha/plugins/ac2wave/ac2wave.cpp
+++ mha/plugins/ac2wave/ac2wave.cpp
@@ -18,9 +18,24 @@
 #include "mha_plugin.hh"
 #include "mha_signal.hh"
 #include "mha_events.h"
+#include <memory>
 
+/** Namespace containing all code for the ac2wave plugin */
+namespace ac2wave {
+
+/** ac2wave real-time configuration class */
 class ac2wave_t {
 public:
+    /** Constructor of the real-time configuration
+     * @param frames_ Number of frames in the input signal
+     * @param channels_ Number of channels in the input signal 
+     * @param ac_ Handle to AC space
+     * @param name_ Name of the AC variable to be mixed in
+     * @param gain_in_ Linear gain for the input signal
+     * @param gain_ac_ Linear gain for the AC input
+     * @param delay_in_ Delay, in fragments, for the input signal
+     * @param delay_ac_ Delay, in fragments, for the AC input
+     */
     ac2wave_t(unsigned int frames_,
               unsigned int channels_, 
               MHA_AC::algo_comm_t & ac_, 
@@ -29,16 +44,29 @@ public:
               float gain_ac_,
               unsigned int delay_in_,
               unsigned int delay_ac_);
-    mha_wave_t* process(mha_wave_t*);
+    /** Process callback
+     * @param s Pointer to input waveform
+     * @returns Pointer to output waveform
+     */
+    mha_wave_t* process(mha_wave_t* s);
 private:
+    /// frames_ Number of frames in the input signal
     unsigned int frames;
+    /// Number of channels in the input signal 
     unsigned int channels;
+    /// AC input waveform
     mha_wave_t w;
+    /// Handle to AC space
     MHA_AC::algo_comm_t & ac;
+    /// Name of the input AC variable
     std::string name;
+    /// Delay, in fragments, for the input signal
     MHASignal::delay_wave_t delay_in;
+    /// Delay, in fragments, for the AC input
     MHASignal::delay_wave_t delay_ac;
+    /// Linear gain for the input signal
     mha_real_t gain_in;
+    /// Linear gain for the AC input
     mha_real_t gain_ac;
 };
 
@@ -83,23 +111,45 @@ mha_wave_t* ac2wave_t::process(mha_wave_t* s)
     return s_in_delayed;
 }
 
+/** ac2wave plugin interface class */
 class ac2wave_if_t : public MHAPlugin::plugin_t<ac2wave_t> {
 public:
+    /** Constructor.
+     * @param iac Handle to the AC space
+     * @param configured_name Assigned name of the plugin within the configuration tree
+     */
     ac2wave_if_t(MHA_AC::algo_comm_t & iac,
                  const std::string & configured_name);
+    /** process callback for spectral input */
     mha_wave_t* process(mha_spec_t*);
+    /** process callback for waveform input
+     * @param s input Pointer to input waveform
+     * @returns Pointer to output waveform. Points to an internal buffer of s_in_delayed
+     */
     mha_wave_t* process(mha_wave_t*);
-    void prepare(mhaconfig_t&);
-    void release();
+    /** Prepare callback. Changes the output domain to waveform, using the
+     * input dimensions in tf.
+     * @param tf Input signal configuration
+     */
+    void prepare(mhaconfig_t& tf);
+    /** Release callback */
+    void release(){};
 private:
+    /** Update the real-time configuration after a configuration change */
     void update();
+    /// AC variable name
     MHAParser::string_t name;
+    /// Linear gain for main input signal
     MHAParser::float_t gain_in;
+    /// Linear gain for AC input signal
     MHAParser::float_t gain_ac;
+    /// Delay of main input signal in fragments
     MHAParser::int_t delay_in;
+    /// Delay of AC input signal in fragments
     MHAParser::int_t delay_ac;
-    MHASignal::waveform_t* zeros;
-    bool prepared;
+    /// Zero signal, used for Spectral inputs
+    std::unique_ptr<MHASignal::waveform_t> zeros;
+    /// Patchbay for connecting the configuration parsers to the update callback
     MHAEvents::patchbay_t<ac2wave_if_t> patchbay;
 };
 
@@ -109,14 +159,13 @@ ac2wave_if_t::ac2wave_if_t(MHA_AC::algo_comm_t & iac, const std::string &)
         "variables. Main and AC signal can be attenuated or delayed\n"
         "by integer fragments. The AC variable and the input waveform have to\n"
         "have the same dimensions.\n\n"
-        "Spectral input is discarded and replaced by a zero signal.\n\n",iac),
+        "Spectral input is discarded and replaced by a zero signal before"
+        " the AC input is mixed in.\n\n",iac),
       name("AC variable name",""),
       gain_in("Linear gain for main input signal","0"),
       gain_ac("Linear gain for AC input signal","1"),
       delay_in("Delay of main input signal in fragments","0","[0,["),
-      delay_ac("Delay of AC input signal in fragments","0","[0,["),
-      zeros(NULL),
-      prepared(false)
+      delay_ac("Delay of AC input signal in fragments","0","[0,[")
 {
     insert_item("name",&name);
     insert_item("gain_in",&gain_in);
@@ -129,7 +178,7 @@ ac2wave_if_t::ac2wave_if_t(MHA_AC::algo_comm_t & iac, const std::string &)
 mha_wave_t* ac2wave_if_t::process(mha_spec_t*)
 {
     poll_config();
-    return cfg->process(zeros);
+    return cfg->process(zeros.get());
 }
 
 mha_wave_t* ac2wave_if_t::process(mha_wave_t* s)
@@ -140,7 +189,7 @@ mha_wave_t* ac2wave_if_t::process(mha_wave_t* s)
 
 void ac2wave_if_t::update()
 {
-    if( prepared )
+    if( is_prepared() )
         push_config(new ac2wave_t(tftype.fragsize,
                                   tftype.channels,
                                   ac,name.data,
@@ -154,23 +203,22 @@ void ac2wave_if_t::prepare(mhaconfig_t& tf)
 {
     tf.domain = MHA_WAVEFORM;
     tftype = tf;
-    zeros = new MHASignal::waveform_t(tf.fragsize,tf.channels);
-    prepared = true;
+    zeros = std::make_unique<MHASignal::waveform_t>(tf.fragsize,tf.channels);
     update();
 }
 
-void ac2wave_if_t::release()
-{
-    prepared = false;
-    delete zeros;
-}
-
-MHAPLUGIN_CALLBACKS(ac2wave,ac2wave_if_t,wave,wave)
-MHAPLUGIN_PROC_CALLBACK(ac2wave,ac2wave_if_t,spec,wave)
+} //namespace ac2wave
+MHAPLUGIN_CALLBACKS(ac2wave,ac2wave::ac2wave_if_t,wave,wave)
+MHAPLUGIN_PROC_CALLBACK(ac2wave,ac2wave::ac2wave_if_t,spec,wave)
 MHAPLUGIN_DOCUMENTATION\
 (ac2wave,
  "data-flow algorithm-communication",
- "")
+ "The 'ac2wave' plugin mixes the input signal with a waveform stored in an AC variable."
+ " The input waveform and the AC signal can be independently attenuated by a linear gain and/or"
+ " delayed by integer fragments. The AC input signal and the main input signal need to have the same"
+ " dimensions. In the case of spectral input, the last time-domain signal dimensions before the transformation"
+ " are used."
+ " Spectral input is discarded and replaced by a zero signal before the AC input is mixed in.\n\n")
 
 // Local Variables:
 // compile-command: "make"
