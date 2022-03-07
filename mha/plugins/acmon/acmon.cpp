71752d722a917113ea48f6210f3ed9fc687bb86e
Paul Maanen <paul.maanen@gmail.com>
2022-03-07 14:44:34 +0100

    Add documentation to acmon
    Summary:
    acmon was missing doxygen comments and a proper manual entry, added
    some documentation. Fixes a minor memory leak while at it.
    
    Closes T78
    Closes T1814
    
    Test Plan: Review generated documentation, doxygen, run `valgrind --leak-check=full mha iolib=MHAIODummy mhalib=mhachain mha.algos="[save_wave:s1 save_wave:s2 acmon]" cmd=start cmd=quit` before and after.
    
    Reviewers: tobias
    
    Reviewed By: tobias
    
    Maniphest Tasks: T78, T1814
    
    Differential Revision: https://dev.openmha.org/D692

--- mha/plugins/acmon/acmon.cpp
+++ mha/plugins/acmon/acmon.cpp
@@ -19,34 +19,59 @@
 #include "mha_signal.hh"
 #include "mha_defs.h"
 #include <math.h>
+#include <memory>
 #include "ac_monitor_type.hh"
 
+
+/** Namespace containing the acmon plugin interface class */
 namespace acmon {
+
+    /** acmon plugin interface class */
 class acmon_t : public MHAPlugin::plugin_t<int> {
 public:
+    /** Plugin interface constructor.
+     * @param configured_name Assigned name of the plugin within the configuration tree
+     */
     acmon_t(MHA_AC::algo_comm_t &, const std::string & configured_name);
-    ~acmon_t();
+    /** Default destructor*/
+    ~acmon_t()=default;
+    /** Prepare callback. Initializes the AC to monitor bridges and variable list, leaves the signal dimensions untouched*/
     void prepare(mhaconfig_t&);
+    /** Do-nothing release */
     void release() {};
+    /** Process callback for frequency domain. Calls save_vars() and returns the signal unmodified.*/
     mha_spec_t* process(mha_spec_t*);
+    /** Process callback for time domain. Calls save_vars() and returns the signal unmodified.*/
     mha_wave_t* process(mha_wave_t*);
 private:
+    /** Save the current value of the AC variables into their corresponding monitors. */
     void save_vars();
+    /** Set/Re-Set the recording mode from continous to snapshot or vice versa */
     void update_recmode();
+    /** Handle to the AC space */
     MHA_AC::algo_comm_t & ac;
+    /** Monitor variable containing the AC variable names */
     MHAParser::vstring_mon_t varlist;
+    /** Monitor variable containing the dimension strings of the AC variables */
     MHAParser::vstring_mon_t dimensions;
+    /** Configuration variable for the display mode */
     MHAParser::kw_t dispmode;
+    /** Configuration variable for the record mode */
     MHAParser::kw_t recmode;
-    std::vector<ac_monitor_t*> vars;
+    /** Vector containing pointers to the ac to monitor bridge variables. This plugin does not clean up after itself */
+    std::vector<std::unique_ptr<ac_monitor_t>> vars;
+    /** Patchbay*/
     MHAEvents::patchbay_t<acmon_t> patchbay;
+    /** String saving the configured name of the plugin */
     std::string algo;
+    /** Recording mode. True for continous mode, false for snapshot mode */
     bool b_cont;
+    /** Snapshot flag. Set to true to request a snapshot in the next process() call */
     bool b_snapshot;
 };
 
 acmon_t::acmon_t(MHA_AC::algo_comm_t & iac, const std::string& configured_name)
-    : MHAPlugin::plugin_t<int>("This algorithm converts AC variables into parsable monitor variables.",iac),
+    : MHAPlugin::plugin_t<int>("This plugin converts AC variables into parsable monitor variables.",iac),
       ac(iac),
       varlist("complete list of variables"),
       dimensions("variable dimensions in AC space"),
@@ -64,10 +89,6 @@ acmon_t::acmon_t(MHA_AC::algo_comm_t & iac, const std::string& configured_name)
     patchbay.connect(&recmode.writeaccess,this,&acmon_t::update_recmode);
 }
 
-acmon_t::~acmon_t(void)
-{
-}
-
 void acmon_t::update_recmode()
 {
     b_cont = (recmode.data.get_index()==0);
@@ -79,17 +100,11 @@ void acmon_t::prepare(mhaconfig_t&)
     const std::vector<std::string> & entrl = ac.get_entries();
     varlist.data.clear();
     dimensions.data.clear();
-    unsigned int k;
-    ac_monitor_t* tmp;
-    for(k=0;k<vars.size();k++){
-        delete vars[k];
-    }
     vars.clear();
-    for(k=0;k<entrl.size();k++){
-        tmp = new ac_monitor_t(*((MHAParser::parser_t*)this),entrl[k],ac,dispmode.data.get_index()!=0);
-        vars.push_back(tmp);
-        varlist.data.push_back(tmp->name);
-        dimensions.data.push_back(tmp->dimstr);
+    for(const auto & entry : entrl){
+        vars.emplace_back(std::make_unique<ac_monitor_t>(*((MHAParser::parser_t*)this),entry,ac,dispmode.data.get_index()!=0));
+        varlist.data.push_back(vars.back()->name);
+        dimensions.data.push_back(vars.back()->dimstr);
     }
     save_vars();
 }
@@ -114,14 +129,22 @@ void acmon_t::save_vars()
     b_snapshot = false;
 }
 
-}
+} //namespace acmon
 
 MHAPLUGIN_CALLBACKS(acmon,acmon::acmon_t,spec,spec)
 MHAPLUGIN_PROC_CALLBACK(acmon,acmon::acmon_t,wave,wave)
 MHAPLUGIN_DOCUMENTATION\
 (acmon,
  "data-export network-communication",
- "")
+ "This plugin converts AC variables into parsable monitor variables. It publishes a monitor variable"
+ " containing the name of every AC variable, a monitor variable containing the dimensions of every AC"
+ " variable and a monitor variable corresponding to every displayable AC variable. Currently all AC"
+ " variables of numerical types and character types are supported. \n\n The monitor variables are either"
+ " updated continously (recmode=\"cont\") or on request (recmode=\"snapshot\"). To request a snapshot"
+ " set recmode to \"snapshot\". Multidimensional AC variables can either be displayed linearized, i.e. as vector,"
+ " or as matrix, using the stride and number of entries to determine the number of rows and columns."
+ " This behavior is controlled by the \"dismode\" configuration variable. The size information monitor is"
+ " not updated after the prepare command.")
 
 // Local Variables:
 // compile-command: "make"
